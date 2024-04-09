f6089d1ca3c6b4920908b3809a7e7d3c7e87e4c1
Mihail Butvin <butvin.mihail@yandex.ru>
2024-04-09 18:21:38 +0300

    Refactor tests

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "Preparing test directory..."
 
 mkdir -p test_tmp
 rm -rf test_tmp/* test_tmp/.gitik
@@ -11,14 +10,11 @@ echo "world" > uwu/world.txt
 echo "nya" > uwu/nya.txt
 
 #
-echo ""
-echo "Testing gitik/help.sh..."
-../gitik/help.sh
+echo "../gitik/help.sh" && ../gitik/help.sh
 
 #
 echo ""
-echo "Testing gitik/init.sh..."
-../gitik/init.sh << EOF
+echo "../gitik/init.sh" && ../gitik/init.sh << EOF
 Mikhail
 butvinm@ya.ru
 Omsk
@@ -26,17 +22,15 @@ EOF
 
 #
 echo ""
-echo "Testing gitik/wtf.sh..."
 touch new_file_0
 touch new_file_1
 rm hello.txt
 rm uwu/world.txt
 echo "nya :3" > uwu/nya.txt
-../gitik/wtf.sh
 
+echo "../gitik/wtf.sh" && ../gitik/wtf.sh
 
 #
 echo ""
-echo "Testing gitik/incest.sh..."
-../gitik/incest.sh
-../gitik/wtf.sh
+echo "../gitik/incest.sh" && ../gitik/incest.sh
+echo "../gitik/wtf.sh" && ../gitik/wtf.sh
